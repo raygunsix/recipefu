2330f694ea55021260c92fc865e9dc507fa60486
Chris Reid <chris.reid@suite101.com>
2011-08-18 21:05:02 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.10'
+gem 'sqlite3'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# gem 'ruby-debug19', :require => 'ruby-debug'
+
+# Bundle the extra gems:
+# gem 'bj'
+# gem 'nokogiri'
+# gem 'sqlite3-ruby', :require => 'sqlite3'
+# gem 'aws-s3', :require => 'aws/s3'
+gem 'jquery-rails'
+gem 'omniauth'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'factory_girl_rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recipefoo::Application.config.secret_token = 'ba4941844de0b49a7d7a04603f507d1178f83685b67ea7ac340a38c6bf786a2f9d02be0abeb2c8e8a533d347f445ede1c267c4027d898cea4db73f08aa1875cf'
