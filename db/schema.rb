9b778d20402b6c7e5ef9a73db2bf33135abb2fb1
Chris Reid <chris@raygun6.com>
2011-08-20 20:25:14 -0700

    fixes syntax errors in migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,28 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20110820214157) do
+
+  create_table "ingredients", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "recipes", :force => true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "steps", :force => true do |t|
+    t.integer  "recipe_id"
+    t.integer  "step"
+    t.string   "instructions"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
