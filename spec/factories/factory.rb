fd4ae497586750dfd3da2053e76f2ee87f9cf6f8
Chris Reid <chris@raygun6.com>
2011-08-20 21:59:03 -0700

    adds initial factory

---
+++ spec/factories/factory.rb
@@ -0,0 +1,10 @@
+# Read about factories at http://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+	
+  factory :recipe do
+    title "Chicken Soup"
+    description "Mom's chicken soup recipe. It was my favorite when I was 12."
+  end
+  
+end
