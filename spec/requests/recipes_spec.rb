aea64ee9135c2f7b5c9bfbaf768fbbfc6033c639
Chris Reid <chris@raygun6.com>
2011-08-21 09:54:47 -0700

    replaces controller specs with request specs

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -22,7 +22,6 @@ describe "Recipes" do
     end
   end
 
-  # requires javascript?
   # describe "GET /recipes/new" do
   #   it "should list all recipes" do
   #     get "/recipes/new"
@@ -32,5 +31,39 @@ describe "Recipes" do
   #     response.body.should have_content("Recipe was successfully created.")
   #   end
   # end  
+  # 
+  # describe "GET /recipes/id/edit" do
+  #   it "should allow recipe to be edited" do
+  #     @recipe.save
+  #     get "/recipes/" + @recipe.id.to_s + "/edit"
+  #     fill_in "Title", :with => "Rabbit Stew"
+  #     fill_in "Description", :with => "This is the best stew ever"
+  #     click_button "Update Recipe"
+  #     response.body.should have_content("Recipe was successfully updated.")
+  #   end
+  # end
+ 
+  describe "POST /recipes" do
+    it "should create a new recipe" do
+      post_via_redirect "/recipes", {:title => @recipe.title, :description => @recipe.description}
+      response.body.should include("Recipe was successfully created.")
+    end
+  end 
+  
+  describe "PUT /recipes/id" do
+    it "should update an existing recipe" do
+      @recipe.save
+      put_via_redirect "/recipes/" + @recipe.id.to_s, {:title => "Chicken Soup Extreme", :description => @recipe.description}
+      response.body.should include("Recipe was successfully updated.")
+    end
+  end  
+ 
+  describe "DELETE /recipes/id" do
+    it "should delete an existing recipe" do
+      @recipe.save
+      delete_via_redirect "/recipes/" + @recipe.id.to_s
+      response.body.should_not include("Chicken Soup")
+    end
+  end 
   
 end
