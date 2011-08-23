992de3c7b4bc77fdd8b3f938080266cdb0c523e2
Chris Reid <chris@raygun6.com>
2011-08-22 21:41:18 -0700

    adds user <-> model associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110823041744) do
+ActiveRecord::Schema.define(:version => 20110823043251) do
 
   create_table "ingredients", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20110823041744) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "steps", :force => true do |t|
