a3976ca12815270d1300ef00c6005036c36ca40f
Chris Reid <chris@raygun6.com>
2011-08-23 22:19:56 -0700

    adds user factory

--- spec/factories/factory.rb
+++ spec/factories/factory.rb
@@ -2,6 +2,12 @@
 
 FactoryGirl.define do
 	
+	factory :user do
+	  provider "twitter"
+	  uid "0000"
+	  name "Guy Lafleur"
+  end
+	
   factory :recipe do
     title "Chicken Soup"
     description "Mom's chicken soup recipe. It was my favorite when I was 12."
