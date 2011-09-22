8d9d8dd6cdf23a51dc587dc4d225765b83180d0e
Chris Reid <chris@raygun6.com>
2011-09-21 22:17:04 -0700

    adds facebook auth provider

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
+  provider :facebook, ENV['APP_ID'], ENV['APP_SECRET']  
 end
